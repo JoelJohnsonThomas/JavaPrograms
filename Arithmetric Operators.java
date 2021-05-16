0f0e2ca5c31e3c7ddacda6c07d2ae85a88df41d8
JoelJohnsonThomas <83132383+JoelJohnsonThomas@users.noreply.github.com>
2021-05-16 08:58:00 +0000

    Create Arithmetric Operators.java

---
+++ Arithmetric Operators.java
@@ -0,0 +1,28 @@
+public class add {
+    public static void main(String[] args) {
+        int i , j, k;
+        i = 4;
+        j = 6;
+        k = i + j ;
+        System.out.printf("The Addition of %d and %d is %d\n",i,j,k);
+
+        int a , b , c;
+        a = 2;
+        b = 6;
+        c = a - b;
+        System.out.printf("The Subtraction of %d and %d is %d\n",a,b,c);
+
+        int p,q,r;
+        p = 3;
+        q = 4;
+        r = p * q;
+        System.out.printf("The Multiplication of %d and %d is %d\n",p,q,r);
+
+        int x,y,z;
+        x = 5;
+        y = 3;
+        z = x / y;
+        System.out.printf("The Division of %d and %d is %d\n",x,y,z);
+    }
+
+    }
